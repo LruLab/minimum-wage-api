9a3b0dc94be1771f16211379751d31f217531df6
KL-Lru <kilattoeruru@outlook.jp>
2023-12-06 03:29:26 +0900

    update meta / favicon

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # build docs
-npx @redocly/cli build-docs -o doc/index.html
+npx @redocly/cli build-docs -t template.hbs -o doc/index.html
 
 # export requirements.txt
 poetry export --without-hashes -o requirements.txt
